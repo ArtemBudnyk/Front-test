b0914f9b5aa9d5c0c81cd67916fd63524f4f6196
ArtemBudnyk <qrtleo@gmail.com>
2024-07-02 15:18:26 +0300

    us-info desktop and mobile done 1:20

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,26 +1,26 @@
 
-// slider 
+// // slider 
 
-$('.slider-template').slick({
-    dots: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 2,
-        }
-      },
-      {
-        breakpoint: 575,
-        settings: {
-          slidesToShow: 1,
-          dots: true,
-          arrows: false,
-        }
-      },
-    ]
-  });
+// $('.slider-template').slick({
+//     dots: false,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     responsive: [
+//       {
+//         breakpoint: 1200,
+//         settings: {
+//           slidesToShow: 1,
+//         }
+//       },
+//       {
+//         breakpoint: 575,
+//         settings: {
+//           slidesToShow: 1,
+//           dots: true,
+//           arrows: false,
+//         }
+//       },
+//     ]
+//   });
