25a2b47327f6264ea61230fc87bd1fd7d397b789
ArtemBudnyk <qrtleo@gmail.com>
2024-07-06 16:19:00 +0300

    slider bag fix, header bag fix

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -24,3 +24,51 @@
 //       },
 //     ]
 //   });
+
+
+// mobileOnlySlider(".slider-template", false, false, 1200);
+
+// function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+//   var slider = $($slidername);
+//   var settings = {
+//     mobileFirst: true,
+//     dots: $dots,
+//     arrows: $arrows,
+//     responsive: [
+//       {
+//         breakpoint: $breakpoint,
+//         settings: "unslick"
+//       }
+//     ]
+//   };
+
+//   slider.slick(settings);
+
+//   $(window).on("resize", function () {
+//     if ($(window).width() > $breakpoint) {
+//       return;
+//     }
+//     if (!slider.hasClass("slick-initialized")) {
+//       return slider.slick(settings);
+//     }
+//   });
+// }
+
+$('.slider-template').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 1,
+          dots: false,
+          arrows: false,
+        }
+      },
+    ]
+  });
